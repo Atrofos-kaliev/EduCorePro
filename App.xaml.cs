8903d6b46a912d08da53e32b05c17c95b1f3a570
Atrofos-kaliev <kalievv01a@gmail.com>
2026-04-21 08:48:37 +0500

    starting project

---
+++ App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows;
+
+namespace EduCorePro;
+
+public partial class App : Application
+{
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        this.DispatcherUnhandledException += (sender, args) =>
+        {
+            MessageBox.Show($"Ошибка UI:\n{args.Exception.Message}\n\n{args.Exception.StackTrace}", "Критическая ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            args.Handled = true;
+        };
+
+        AppDomain.CurrentDomain.UnhandledException += (sender, args) =>
+        {
+            var ex = (Exception)args.ExceptionObject;
+            MessageBox.Show($"Фатальная ошибка:\n{ex.Message}\n\n{ex.StackTrace}", "Фатальная ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+        };
+
+        base.OnStartup(e);
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using MudBlazor.Services;
+
+namespace EduCorePro;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        try
+        {
+            InitializeComponent();
+
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection.AddWpfBlazorWebView();
+
+            serviceCollection.AddMudServices(); 
+
+            Resources.Add("services", serviceCollection.BuildServiceProvider());
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show($"Произошла ошибка при запуске:\n\n{ex.Message}\n\n{ex.StackTrace}", 
+                            "Ошибка EduCore Pro", 
+                            MessageBoxButton.OK, 
+                            MessageBoxImage.Error);
+        }
+    }
+}
